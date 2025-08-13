471c7fa7014ad686710aef8c99640d570793ae3f
EduardoFen12 <dudufen03@gmail.com>
2025-08-13 16:02:26 -0300

    Inicialização do projeto

---
+++ API-Challange/API-Challange/API_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ChallangeApp.swift
+//  API-Challange
+//
+//  Created by Eduardo Garcia Fensterseifer on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct API_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API-Challange/API-Challange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API-Challange
+//
+//  Created by Eduardo Garcia Fensterseifer on 13/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
