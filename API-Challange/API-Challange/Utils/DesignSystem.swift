18c9ffb7d62471ba3e55be6b1d6f987c672f82d3
Gustavo <gustavo_fbassani@hotmail.com>
2025-08-13 16:06:08 -0300

    inicial Config

---
+++ API-Challange/API-Challange/Utils/DesignSystem.swift
@@ -0,0 +1,31 @@
+//
+//  DesignSystem.swift
+//  API-Challange
+//
+//  Created by Gustavo Ferreira bassani on 13/08/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundPrimary = Color("backgroundPrimary")
+    static let backgroundSecondary = Color("backgroundSecondary")
+    static let backgroundTertiary = Color("backgroundTertiary")
+    
+    static let bluePrimary = Color("bluePrimary")
+    
+    static let fillPrimary = Color("fillPrimary")
+    static let fillSecondary = Color("fillSecondary")
+    static let fillTertiary = Color("fillTertiary")
+    static let fillQuaternary = Color("fillQuaternary")
+    
+    static let gray2 = Color("gray2")
+    static let gray5 = Color("gray5")
+    
+    static let labelPrimary = Color("labelPrimary")
+    static let labelSecondary = Color("labelSecondary")
+    static let labelTertiary = Color("labelTertiary")
+    
+    static let separatorOpaque = Color("separatorOpaque")
+    static let separatorNonOpaque = Color("separatorNonOpaque")
+}
