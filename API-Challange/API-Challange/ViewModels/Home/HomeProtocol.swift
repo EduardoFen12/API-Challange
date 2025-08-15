d1a85fcd50254c7212762f1940ba3788225c4c86
Gustavo <gustavo_fbassani@hotmail.com>
2025-08-15 16:41:21 -0300

    HomeModelView done

--- API-Challange/API-Challange/ViewModels/Home/HomeProtocol.swift
+++ API-Challange/API-Challange/ViewModels/Home/HomeProtocol.swift
@@ -4,4 +4,14 @@
 //
 //  Created by Gustavo Ferreira bassani on 15/08/25.
 //
+import Foundation
 
+protocol HomeProtocol {
+    
+    var dealPickProduct: ProductModel? { get }
+    var products: [ProductModel] { get }
+    var isLoading: Bool { get }
+    var errorMessage: String? { get }
+    func loadProducts() async
+    
+}
