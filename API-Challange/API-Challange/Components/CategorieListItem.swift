8c9e7f716ff4abf7dadb6d9276d6b35556bc25e5
Gustavo <gustavo_fbassani@hotmail.com>
2025-08-14 23:05:01 -0300

    CategoriesView screen done

---
+++ API-Challange/API-Challange/Components/CategorieListItem.swift
@@ -0,0 +1,37 @@
+//
+//  CategorieListItem.swift
+//  API-Challange
+//
+//  Created by Gustavo Ferreira bassani on 14/08/25.
+//
+
+import SwiftUI
+
+struct CategorieListItem: View {
+    
+    @State var number: Int
+    
+    var body: some View {
+        
+        HStack {
+            Text("Category \(number)")
+                .font(.body)
+                .foregroundStyle(.labelsPrimary)
+                .padding(.leading, 16)
+            
+            Spacer()
+            
+            Image(systemName: "chevron.right")
+                .foregroundStyle(.labelsTertiary)
+                .fontWeight(.semibold)
+                .padding(.trailing, 16)
+                
+        }
+        .frame(height: 60)
+    }
+    
+}
+
+#Preview {
+    CategorieListItem(number: 1)
+}
---
+++ API-Challange/API-Challange/Screens/CategoriesView.swift
@@ -0,0 +1,51 @@
+//
+//  Categories.swift
+//  API-Challange
+//
+//  Created by Gustavo Ferreira bassani on 14/08/25.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    @State private var searchText = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 16) {
+                ScrollView {
+                    HStack(spacing:8) {
+                        CategoriesCard()
+                        CategoriesCard()
+                        CategoriesCard()
+                        CategoriesCard()
+                        
+                    }
+                    .padding(.top, 16)
+                    
+                    ForEach(1..<8) { index in
+                        CategorieListItem(number: index)
+
+                            .listRowSeparator(index == 0 ? .hidden : .visible)
+                            Divider()
+                            .padding(.leading, 16)
+                            .frame(height: 1)
+                                    .padding(.vertical, 0)
+                    }
+                    
+                    
+                }
+                .navigationTitle("Categories")
+                .searchable(text: $searchText)
+                
+                
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    TabBar()
+}
+
